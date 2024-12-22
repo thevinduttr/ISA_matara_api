70d3e8e6fcbab6e00d01563bc7dd0e91b3686b9a
Thevindu Rathnaweera <rathnaweerathevindu2003@gmail.com>
2024-12-22 14:19:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config');
+const galleryRoutes = require('./routes/galleryRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const contactRoutes = require('./routes/contactRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+require('dotenv').config();
+
+const app = express();
+
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+// Use auth routes
+app.use('/api', authRoutes);
+
+// Routes
+app.use('/api/gallery', galleryRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/contact', contactRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
